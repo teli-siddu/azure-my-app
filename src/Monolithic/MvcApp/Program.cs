62c7e505ceb74039901e8bd2608baabf7c835926
teli-siddu <teli.siddu.n@gmail.com>
2024-07-21 05:02:15 +0530

    jjj

--- src/Monolithic/MvcApp/Program.cs
+++ src/Monolithic/MvcApp/Program.cs
@@ -5,11 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddAzureAppConfiguration();
-builder.Configuration.AddAzureAppConfiguration(options =>
-{
-    var con = builder.Configuration.GetValue<string>("Demo:ConnectionString");
-    options.Connect(con);
-});
 
 // Build configuration
 builder.Configuration.AddAzureAppConfiguration(options =>
